f62bc3cf8600609b7f5aaa2462d0790dedcd661d
Jordan J <jordanr2m@gmail.com>
2023-10-25 12:16:28 -0700

    Fixed final bug with JS.

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ const errorIcon = document.querySelector("#error-icon");
 const errorMsg = document.querySelector(".error-message");
 
 submitButton.addEventListener("click", function (e) {
-    const acceptedCharacters = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+    const acceptedEmail = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
     const userEmail = userInput.value;
-    if (!userEmail.match(acceptedCharacters) || userEmail.length === 0) {
+
+    if (!userEmail.match(acceptedEmail) || userEmail.length === 0) {
+        e.preventDefault()
         userInput.style.border = "1px solid hsl(0, 93%, 68%)";
         errorIcon.classList.remove("hidden");
         errorMsg.classList.remove("hidden");
-        e.preventDefault()
+    } else {
+        userInput.style.border = "1px solid hsl(0, 36%, 70%);";
+        errorIcon.classList.add("hidden");
+        errorMsg.classList.add("hidden");
     }
 });
 
