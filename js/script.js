96be00643ba168a3d2c85ac9fdf8fe7cd5b9b776
Jordan J <jordanr2m@gmail.com>
2023-10-25 11:05:19 -0700

    Finishing touches- cleaned up JS code.

--- js/script.js
+++ js/script.js
@@ -6,15 +6,11 @@ const errorMsg = document.querySelector(".error-message");
 submitButton.addEventListener("click", function (e) {
     const acceptedCharacters = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
     const userEmail = userInput.value;
-    if (!userEmail.match(acceptedCharacters)) {
+    if (!userEmail.match(acceptedCharacters) || userEmail.length === 0) {
         userInput.style.border = "1px solid hsl(0, 93%, 68%)";
         errorIcon.classList.remove("hidden");
         errorMsg.classList.remove("hidden");
-    } else {
         e.preventDefault();
-        userInput.style.border = "1px solid hsl(0, 36%, 70%)";
-        errorIcon.classList.add("hidden");
-        errorMsg.classList.add("hidden");
     }
 });
 
